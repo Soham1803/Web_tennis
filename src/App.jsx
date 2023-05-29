dfde653d32b8c05ca0e11df1df14dc21f5dad633
Soham <mastpanchalsoham@gmail.com>
2023-05-29 20:39:22 +0530

    Added purple directional light and turned on the animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
 
-
-import { OrbitControls } from '@react-three/drei'
-import Game_world from './pages/Game_world'
-
+import { OrbitControls } from "@react-three/drei";
+import Game_world from "./pages/Game_world";
 
 function App() {
   return (
-    <Canvas className='canvas' camera={{fov: 75, near: 0.1, far: 5000, position: [0, 8, 15]}} >
+    <Canvas
+      className="canvas"
+      camera={{ fov: 75, near: 0.1, far: 5000, position: [0, 8, 15] }}
+      shadows
+      colorManagement
+    >
       <OrbitControls />
       <Game_world />
-      
-        
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Game_world.jsx
+++ src/pages/Game_world.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import { Suspense, useState } from 'react'
-import { Environment } from '@react-three/drei'
+import React from "react";
+import { Suspense, useState, useRef } from "react";
+import { Environment, useHelper } from "@react-three/drei";
 
-import Tennis_animation_compressed from '../components/models_components/Tennis_animation_compressed'
+import Tennis_animation_compressed from "../components/models_components/Tennis_animation_compressed";
+
+import { DirectionalLight, DirectionalLightHelper } from "three";
 
 const Game_world = () => {
+
+  // const dirLight = useRef<DirectionalLight>(null);
+  // useHelper(dirLight, DirectionalLightHelper, 1, "red");
+
   return (
     <>
-        <Environment files='environment/purple_sky.hdr' background blur={0} />
+      <Environment files="environment/purple_sky.hdr" background blur={0} />
+      <directionalLight
+        color="purple"
+        position={[0, 7, 10]}
+        castShadow
+        shadow-mapSize-height={512}
+        shadow-mapSize-width={512}
+        // ref={dirLight}
+      />
 
-        <Suspense >
-          <Tennis_animation_compressed />
-        </Suspense>
-        {/* <mesh>
+      {/* <axesHelper args={[1000]} />
+      <gridHelper args={[1000]} /> */}
+
+      <Suspense>
+        <Tennis_animation_compressed />
+      </Suspense>
+      {/* <mesh>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh> */}
-        {/* <ambientLight /> */}
-        <hemisphereLight color='yellow' groundColor='blue' />
+      {/* <ambientLight /> */}
+      <hemisphereLight color="yellow" groundColor="blue" />
     </>
-  )
-}
+  );
+};
 
-export default Game_world
+export default Game_world;
